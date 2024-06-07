7e9f645b690099c36639bde0c645513dfe999cca
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-06 22:18:59 -0300

    bleh

---
+++ api/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.MapGet("/", () => "Hello World!");
+
+
+
+
+
+app.Run();
